1a067682bb26fbe2882dc2d71be6aa83fd526875
neozod <manish@neozod.com>
2020-07-02 22:25:42 +1000

    Update MS.psm1

--- MS.psm1
+++ MS.psm1
@@ -1,16 +1,1 @@
-﻿function anydesk(){
-    param($string)
-    Start-Process "E:\code\CODE\bginfo\anydesk\AnyDesk_$String.msi" -ArgumentList '/quiet' -Wait
-
-}
-
-function Write-ProgressHelper {
-	param (
-		[int]$StepNumber,
-		[string]$Message,
-        [string]$Title
-	)
-
-	Write-Progress -Activity $Title -Status $Message -PercentComplete (($StepNumber / $steps) * 100)
-}
 
