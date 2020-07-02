7e9e320f59f54f1e0323749ff66bc88b98baca31
neozod <manish@neozod.com>
2020-07-02 22:23:10 +1000

    Add files via upload

---
+++ MS.psm1
@@ -0,0 +1,16 @@
+﻿function anydesk(){
+    param($string)
+    Start-Process "E:\code\CODE\bginfo\anydesk\AnyDesk_$String.msi" -ArgumentList '/quiet' -Wait
+
+}
+
+function Write-ProgressHelper {
+	param (
+		[int]$StepNumber,
+		[string]$Message,
+        [string]$Title
+	)
+
+	Write-Progress -Activity $Title -Status $Message -PercentComplete (($StepNumber / $steps) * 100)
+}
+
